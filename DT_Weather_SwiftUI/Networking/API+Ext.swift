550c80afb1b8566544ddc84bd51fb964ed20f24b
Cory Tepper <corytepper@gmail.com>
2022-12-05 20:32:11 -0500

    API, API+Ext and generic NetworkManager

--- DT_Weather_SwiftUI/Networking/API+Ext.swift
+++ DT_Weather_SwiftUI/Networking/API+Ext.swift
@@ -8,10 +8,11 @@
 import Foundation
 
 extension API {
+    
     static let baseURLString = "https://api.openweathermap.org/data/3.0"
+    
     
     static func getURLFor(lat: Double, lon: Double) -> String {
         return "\(baseURLString)onecall?lat=\(lat)&lon=\(lon)&exclude=minutely&appid=\(key)&units=imperial"
     }
-    
 }
