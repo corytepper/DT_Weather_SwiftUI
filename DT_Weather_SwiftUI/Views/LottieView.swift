f96a011680c18113d727f0d087aec9801340f27d
Cory Tepper <corytepper@gmail.com>
2022-12-08 15:05:39 -0500

    Lottie pkg added and LottieView built

---
+++ DT_Weather_SwiftUI/Views/LottieView.swift
@@ -0,0 +1,38 @@
+//
+//  LottieView.swift
+//  DT_Weather_SwiftUI
+//
+//  Created by Cory Tepper on 12/8/22.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> some UIView {
+        let view = UIView()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: UIViewRepresentableContext<LottieView>) {
+        uiView.subviews.forEach({$0.removeFromSuperview()})
+        
+        let animationView = LottieAnimationView()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        uiView.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: uiView.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: uiView.heightAnchor)
+        ])
+        
+        animationView.animation = LottieAnimation.named(name)
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = loopMode
+        animationView.play()
+    }
+    
+}
